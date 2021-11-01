849efe1759036e9d973f81e8316ca06420584cef
Kai Martins-Turner <martins-turner@vsp.tu-berlin.de>
2021-11-01 14:41:30 +0100

    Nun mit GoogleJuice Inject

--- src/main/java/org/matsim/VL03DependencyInjection/RunSimulation.java
+++ src/main/java/org/matsim/VL03DependencyInjection/RunSimulation.java
@@ -1,11 +1,30 @@
 package org.matsim.VL03DependencyInjection;
 
+import com.google.inject.*;
+import com.google.inject.Module;
+
 class RunSimulation{
 
 	public static void main( String[] args ){
-		Helper helper = new HelperDefaultImpl();
-		Simulation simulation = new SimulationDefaultImpl( helper );
+		// calssic way: dependenc< incetion via constructor
+//		Helper helper = new HelperDefaultImpl();
+//		Simulation simulation = new SimulationDefaultImpl( helper );
+//		simulation.doStep();
+		
+//		Using guice
+
+		Module module = new AbstractModule() { //Hier die Google-Variante nehmen, weil wir noch ohne MATSim arbeiten.
+			@Override
+			protected void configure() {
+				this.bind(Simulation.class).to(SimulationDefaultImpl.class); //Binde Interface TO Implementation.
+				this.bind(Helper.class).to(HelperDefaultImpl.class); // das was vorher das war: Helper helper = new HelperDefaultImpl();
+ 			}
+		};
+		Injector injector = Guice.createInjector(module);
+		Simulation simulation = injector.getInstance(Simulation.class);
+		
 		simulation.doStep();
+		
 	}
 	interface Helper{
 		void help();
@@ -19,10 +38,12 @@ class RunSimulation{
 		}
 	}
 	static class SimulationDefaultImpl implements Simulation {
-		private final Helper helper;
-		SimulationDefaultImpl( Helper helper ) {
-			this.helper = helper;
-		}
+		@Inject private Helper helper;
+
+		//remove non-empty constructor to have a zero-argument constructor (for guice...) since not having exactly one @inject annotated constructor
+//		SimulationDefaultImpl( Helper helper ) {
+//			this.helper = helper;
+//		}
 		public void doStep() {
 			System.out.println( "entering " + this.getClass().getSimpleName() );
 			helper.help();
