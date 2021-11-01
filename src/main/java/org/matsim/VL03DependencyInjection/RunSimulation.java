2590c2341104ff930aa149c32650b9e1f5669d82
Kai Martins-Turner <martins-turner@vsp.tu-berlin.de>
2021-11-01 14:52:14 +0100

    Using Option A again

--- src/main/java/org/matsim/VL03DependencyInjection/RunSimulation.java
+++ src/main/java/org/matsim/VL03DependencyInjection/RunSimulation.java
@@ -38,14 +38,14 @@ class RunSimulation{
 		}
 	}
 	static class SimulationDefaultImpl implements Simulation {
-//		//A:) remove non-empty constructor to have a zero-argument constructor (for guice...) since not having exactly one @inject annotated constructor
-//		@Inject private Helper helper;
+		//A:) remove non-empty constructor to have a zero-argument constructor (for guice...) since not having exactly one @inject annotated constructor
+		@Inject private Helper helper;
 
-//	B:	Alternative dazu wäre - ist oft in MATSim, wo wir die bisherigen Construcoren wieder nutzen.:
-		private final Helper helper;
-		@Inject SimulationDefaultImpl(Helper helper){
-			this.helper = helper;
-		}
+////	B:	Alternative dazu wäre - ist oft in MATSim, wo wir die bisherigen Construcoren wieder nutzen.:
+//		private final Helper helper;
+//		@Inject SimulationDefaultImpl(Helper helper){
+//			this.helper = helper;
+//		}
 
 		public void doStep() {
 			System.out.println( "entering " + this.getClass().getSimpleName() );
